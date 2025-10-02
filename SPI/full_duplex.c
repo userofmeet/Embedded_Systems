7f3a5a8f6524f06507f94fcd5c90108666ed69b2
Meet Jain <meetjain538@gmail.com>
2025-10-02 20:13:52 +0530

    Update full_duplex.c

--- SPI/full_duplex.c
+++ SPI/full_duplex.c
@@ -18,7 +18,7 @@ void Initialize_spi(void){
 void Initialize_IOA(void){			// initialize the input and output pins for MOSI, CLOCL and MISO
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;	  // enable the GPIO A
 	GPIOA->CRL = 0xB8B00000;		// configure pin 7,6,5 of A as specified in data sheet
-  //	AFIO->EVCR = 0x87 | 0x85;	// enable the alternate function of pins. fucntion which are to be configured as alternate fucntion
+//	AFIO->EVCR = 0x87 | 0x85;	// enable the alternate function of pins. fucntion which are to be configured as alternate fucntion
 }
 
 
@@ -44,6 +44,7 @@ ch = SPI1->DR;          // reading received value in ch variable
 	ch = SPI1->DR;
 	while(!(SPI1->SR & SPI_SR_RXNE));
 	while(!(SPI1->SR & SPI_SR_BSY)); // wait for line to be un_busy
-  
+	
 	// here we are supposed to disable the spi peripheral
 }
+
